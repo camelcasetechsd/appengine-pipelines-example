962af8f10bdeb20febaa3a2d5194f6bc88f61fbd
Najipian <amr.najip@gmail.com>
2018-11-15 18:45:57 +0200

    show logged user in home template (#1)

--- app/modules/home.py
+++ app/modules/home.py
@@ -2,9 +2,11 @@ import webapp2
 import logging
 import os
 from google.appengine.ext.webapp import template
+from google.appengine.api import users
 
 class HomeHandler(webapp2.RequestHandler):
     def get(self):
         logging.info("HomeHandler get()")
+        user = users.get_current_user()
         template_path = os.path.join(os.path.dirname(__file__), 'home.html')
-        self.response.write(template.render(template_path, {}))
+        self.response.write(template.render(template_path, {'user_name' : user.nickname()}))
