cc2eac33358107d6974f1ea9a62e18fc198fb60c
Najipian <amr.najip@gmail.com>
2018-11-21 14:06:19 +0200

    clean code

--- app/modules/common/kinds.py
+++ app/modules/common/kinds.py
@@ -2,6 +2,5 @@ import webapp2
 import logging
 from google.appengine.ext import ndb
 
-
 class Example(ndb.Model):
     value = ndb.StringProperty()
--- app/modules/home.py
+++ app/modules/home.py
@@ -9,24 +9,20 @@ class HomeHandler(webapp2.RequestHandler):
     def get(self):
         logging.info("HomeHandler get()")
 
-        logging.info(self.request.remote_addr)
+        # get logged in user
+        user = users.get_current_user()
 
+        # get user coordinates via user ip
         g = geocoder.ip(self.request.remote_addr)
         
-
         if not g.latlng:
+            # if user coordinates is not detected locate in cairo
             lat = 30.0355
             lng = 31.223
         else:
             lat = g.latlng[0]
             lng = g.latlng[1]
-            
-        logging.info(lat)
-        logging.info(lng)
         
-        # get logged in user
-        user = users.get_current_user()
-
         template_path = os.path.join(os.path.dirname(__file__), 'home.html')
         
         self.response.write(template.render(template_path, {'user_name' : user.nickname() , 'lat' : lat , 'lng' : lng}))
